f2bddd9929405672a63035706a28642f7d47c184
Bazouboutchou <julienmarie83560@gmail.com>
2017-10-23 12:38:59 +0200

    suite

--- TP/src/tp4/Main.java
+++ TP/src/tp4/Main.java
@@ -1,6 +1,8 @@
 package tp4;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Scanner;
 
@@ -33,43 +35,86 @@ public class Main {
 		}
 		System.out.println("**************B****************");
 		
-		int choix = 0;
+		String choix;
 		int elem = 0;
 		Entier entier = new Entier (10);
-		do{
-			System.out.println("Entrer 0 pour inserer, 1 pour supprimer et -1 pour sortir");
-			choix = sc.nextInt();
-			System.out.println("Entrer l'element");
-			switch (choix){
-				case 0:
-						
-					elem = sc.nextInt();
-
-					entier.inserer(elem);
-					
-					break;
-				case 1:
-					try {
-						
-						elem = sc.nextInt();
-						entier.supprimer(elem);
-					} catch (ExceptionFaible e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					break;
-				default:
-					System.out.println("Mauvais choix");
-					break;
-				
-			}
-			System.out.println(elem);
-		}while (choix != -1);
+		Method m;
+		System.out.println("Entrer inserer ou supprimer et q pour sortir");
+		choix = sc.next();
 		
+		while (!(choix.equals("q"))){
+		
+			System.out.println("Entrer l'element");
+			elem = sc.nextInt();
+
+			try {
+				m = Entier.class.getMethod(choix, int.class);
+				m.invoke(entier, elem);		
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			System.out.println("Entrer inserer ou supprimer et q pour sortir");
+			choix = sc.next();
+		}		
 		System.out.println(entier);
 		
 		System.out.println("**************C****************");
 		
+		Point p = new  Point (1,2);
+		String name = null;
+		System.out.println("Etrer x ou y");
+		name = sc.next();
+		System.out.println(p);
+		try {
+			Point.class.getField(name).set(p, 0);
+		} catch (IllegalArgumentException | IllegalAccessException
+				| NoSuchFieldException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(p);
+		
+		System.out.println("************D**************");
+
+		Constructor<Entier> co;
+		try {
+			co = Entier.class.getConstructor(int.class);
+			Entier a = co.newInstance(10);
+			
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main (String[] args){
