b550b1f5f91d379c4c99662eaffdd5c59c1af2d4
Julien MARIE <julienmarie83560@gmail.com>
2017-11-25 22:31:23 +0100

    tp5 suite

--- TP/src/tp5/Main.java
+++ TP/src/tp5/Main.java
@@ -3,8 +3,8 @@ package tp5;
 public class Main {
 
 	public static void main(String[] args) {
-		IExpression res = new OpAdd(1, , g)
-
+		IExpression res = new OpAdd(true, new ExpressionParenthese(new OpAdd(true, new OpMul(true, new Chiffre(363), new Variable('y')),new Chiffre(2))) , new Chiffre(5));
+		System.out.println("Res de (7/y -2) = " + res.resoudre() + " avec y = 121");
 	}
 
 }
